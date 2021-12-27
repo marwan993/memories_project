9390dee73d98208537a32f86b6d8edfac930453f
Marwan Alaloosi <marwanalaloosi@Marwans-MBP.lan>
2021-12-26 21:54:22 -0600

    added

---
+++ controllers/posts.js
@@ -0,0 +1,58 @@
+import PostMessage from '../models/postMessage.js';
+import Mongoose from 'mongoose';
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = async (req, res) => {
+
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
+}
+
+export const updatePost = async (req, res) => {
+
+    const { id: _id } = req.params;
+    const post = req.body;
+    
+    if(!Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that Id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id}, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+
+    const { id: _id } = req.params;
+
+    if(!Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that Id');
+
+    await PostMessage.findByIdAndRemove(_id);
+
+    res.json('Post deleted successfully!')
+}
+
+export const likePost = async (req, res) => {
+
+    const { id: _id } = req.params;
+
+    if(!Mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that Id');
+
+    const post = await PostMessage.findById(_id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    res.json(updatedPost);
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
